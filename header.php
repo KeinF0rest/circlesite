0169fa0f87b3645d6f3863a30bed42ad74cdcd19
kein <kein10119810@gmail.com>
2025-11-05 03:03:11 +0900

    ヘッダー部分の作成

---
+++ header.php
@@ -0,0 +1,31 @@
+<!-- header.php -->
+<div class="header-top">
+    <div class="site-title">サークル名</div>
+    <div id="navArea">
+        <nav>
+            <div class="inner">
+                <ul>
+                    <li><a href="index.php">カレンダー</a></li>
+                    <li><a href="event.php">イベント</a></li>
+                    <li><a href="#">ブログ</a></li>
+                    <li><a href="#">アルバム</a></li>
+                    <li><a href="account.php">アカウント一覧</a></li>
+                </ul>
+            </div>
+        </nav>
+                
+        <div class="toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <div id="mask"></div>
+    </div>
+</div>
+<div class="header-bottom">
+    <div class="header-buttons">
+        <button onclick="alert('通知ページへ移動')">🔔 通知</button>
+        <button onclick="alert('マイページへ移動')">👤 マイページ</button>
+    </div>
+</div>
