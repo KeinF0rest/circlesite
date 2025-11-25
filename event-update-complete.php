e952688366e052633430d71d1f2886c4f176f3f3
kein <kein10119810@gmail.com>
2025-11-26 07:34:52 +0900

    イベント更新、アクセス権限の追加

--- event-update-complete.php
+++ event-update-complete.php
@@ -6,6 +6,12 @@ if (!isset($_SESSION['user'])) {
     exit();
 }
 
+if ($_SESSION['user']['authority'] == 0) {
+    $_SESSION['error'] = "アクセス権限がありません。";
+    header("Location: index.php");
+    exit();
+}
+
 $pdo = new PDO("mysql:dbname=circlesite;host=localhost;", "root", "");
 
 $title = $_POST['title'] ?? '';
--- event-update.php
+++ event-update.php
@@ -5,6 +5,13 @@ if (!isset($_SESSION['user'])) {
     header("Location: login.php");
     exit();
 }
+
+if ($_SESSION['user']['authority'] == 0) {
+    $_SESSION['error'] = "アクセス権限がありません。";
+    header("Location: index.php");
+    exit();
+}
+
 $pdo = new PDO("mysql:dbname=circlesite;host=localhost;", "root", "");
 $event_id = $_GET['id'] ?? null;
 
