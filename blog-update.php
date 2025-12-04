4642b25aadef246294fb96729901346fe3511ded
kein <kein10119810@gmail.com>
2025-12-05 07:16:36 +0900

    ブログ更新_アカウント権限の修正

--- blog-update.php
+++ blog-update.php
@@ -6,6 +6,12 @@ if (!isset($_SESSION['user'])) {
     exit();
 }
 
+if ($_SESSION['user']['authority'] == 0) {
+    $_SESSION['error'] = "アクセス権限がありません。";
+    header("Location: index.php");
+    exit();
+}
+
 $id = $_GET['id'] ?? null;
 
 $pdo = new PDO("mysql:dbname=circlesite;host=localhost;", "root", "");
