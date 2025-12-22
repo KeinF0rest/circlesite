fbbfd4154de64605ef5937f26964514ae3258d99
kein <kein10119810@gmail.com>
2025-12-22 21:24:02 +0900

    チャット画面_細かい修正

--- chat.php
+++ chat.php
@@ -282,6 +282,7 @@ foreach ($messages as $msg) {
             document.getElementById("image").addEventListener("change", previewImage);
             function previewImage(event) {
                 const file = event.target.files[0];
+                
                 if (file) {
                     const reader = new FileReader();
                     reader.onload = function(e) {
